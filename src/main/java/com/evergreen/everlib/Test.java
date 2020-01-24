7cd47c80b2c1869f7df1b8946322f0885f9235e8
Atai Ambus <atayambus@gmail.com>
2020-01-24 18:13:58 +0200

    feat(test): add constants testing
     - Added boolean, integer, double and string constants, each placed in different folders using the .., / and . syntax and with pushd and popd.
    
    - Specifically, there should be a SpeedModifier double constant at /SubsystemName/Constants, CanMove boolean constnat at /Thingies, Joystick port constant at /Thingies/complicaterd, Remarks String constants a Thingies/Simple, and Path String COnstant at /Thingies.
    
    - When ran, the code should check the shuffleboard Boolean, and if it's true, move the chassis according to the joystick port specified in the shuffleboard, multiplied by the shuffleboard speed modifier. It then should print a statement detailing their values, opening with the value of the string constant.

--- src/main/java/com/evergreen/everlib/Test.java
+++ src/main/java/com/evergreen/everlib/Test.java
@@ -3,13 +3,18 @@ package com.evergreen.everlib;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.evergreen.everlib.oi.joysticks.ExtremeProJoystick;
+import com.evergreen.everlib.shuffleboard.constants.ConstantBoolean;
 import com.evergreen.everlib.shuffleboard.constants.ConstantDouble;
+import com.evergreen.everlib.shuffleboard.constants.ConstantInt;
+import com.evergreen.everlib.shuffleboard.constants.ConstantString;
+import com.evergreen.everlib.shuffleboard.constants.DashboardConstants;
 import com.evergreen.everlib.shuffleboard.loggables.DashboardStreams;
 import com.evergreen.everlib.structure.Tree;
 import com.evergreen.everlib.subsystems.motors.subsystems.MotorController;
 import com.evergreen.everlib.subsystems.motors.subsystems.MotorController.ControllerType;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Joystick;
 
 /**
  * Test
@@ -17,28 +22,43 @@ import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 public class Test extends Tree {
     
     
-    MotorController m_motorRight = new MotorController(
+    MotorController m_chassis = new MotorController( //TODO Check ports!
         "Motor Right",
-        new MotorController("Talon", ControllerType.TALON_SRX, 9),
-        new MotorController("Victor", ControllerType.VICTOR_SPX, 0));
+        new MotorController("Left Front", ControllerType.TALON_SRX, 9),
+        new MotorController("Left Back", ControllerType.VICTOR_SPX, 0),
+        new MotorController("Right Front", ControllerType.TALON_SRX, 1),
+        new MotorController("Rught Back", ControllerType.VICTOR_SPX, 4),
+        new MotorController("Left Up", ControllerType.VICTOR_SPX, 0),
+        new MotorController("Right Up", ControllerType.VICTOR_SPX, 1));
 
-    MotorController m_motorLeft = new MotorController(
-        "Motor Left",
-        new MotorController("Talon", ControllerType.TALON_SRX, 1),
-        new MotorController("Victor", ControllerType.VICTOR_SPX, 4));
+    static {
+        DashboardConstants.getInstance().startConstantsOf("subsystemName");
+    }
 
-    WPI_TalonSRX m_leftA = new WPI_TalonSRX(9);
-    WPI_TalonSRX m_rightA = new WPI_TalonSRX(1);
-    WPI_VictorSPX m_leftB = new WPI_VictorSPX(0);
-    WPI_VictorSPX m_rightB = new WPI_VictorSPX(4);
+    ConstantDouble m_speedModifier = new ConstantDouble("Speed Modifier", 0.5);
+    
+    static {
+        DashboardConstants.getInstance().cd("/Thingies");
+    }
 
-    ConstantDouble m_speedModifier = new ConstantDouble("Speed", 0.5);
-    ConstantDouble m_change = new ConstantDouble("Right Adjust", 1.2);
+    ConstantBoolean m_canMove = new ConstantBoolean("Can Move", true);
 
-    ExtremeProJoystick m_jsLeft = new ExtremeProJoystick("Joystick Left", 0, (v) -> v * 0.5 * 0.85);
-    ExtremeProJoystick m_jsRight = new ExtremeProJoystick("Joystick Right", 1, (v) -> v * 0.5);
+    static {
+        DashboardConstants.getInstance().pushd("./complicated/.");
+    }
 
-    ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+    ConstantInt m_joystickPort = new ConstantInt("Joystick Port", 0);
+
+    static {
+        DashboardConstants.getInstance().cd("../simple");
+    }
+
+    ConstantString m_remarks = new ConstantString("Remarks", "testing.");
+
+    static {
+        DashboardConstants.getInstance().popd();
+        new ConstantString("Path", "Where am I now?");
+    }
   
     @Override
     protected void componentSetup() {
@@ -55,13 +75,6 @@ public class Test extends Tree {
 
     @Override
     protected void log() {
-        // DashboardStreams.addLoggable(m_motorLeft);
-        // DashboardStreams.addLoggable(m_motorRight);
-        // DashboardStreams.addDouble(
-        //     "Chassis Left Encoder", 
-        //     () -> (double)((WPI_TalonSRX)m_motorLeft.getMotors().get(0)).getSelectedSensorPosition());
-        DashboardStreams.addDouble(
-            "Talon Encoder", () -> m_leftA.getSelectedSensorPosition() * 1.0);
     }
 
     @Override
@@ -85,13 +98,14 @@ public class Test extends Tree {
 
     @Override
     public void testPeriodic() {
-        // m_motorLeft.set(m_jsLeft.getY());
-        // m_motorRight.set(m_jsRight.getY());
         
-        m_leftA.set(-m_jsLeft.getY());
-        m_leftB.set(-m_jsLeft.getY());
-        m_rightA.set(m_jsRight.getY());
-        m_rightB.set(m_jsRight.getY());
+        if (m_canMove.get())
+            m_chassis.set(new Joystick(m_joystickPort.get()).getY() * m_speedModifier.get());
+
+        System.out.println(String.format(
+            "%s. \nInteger %s\nDouble %s\nBoolean %s", 
+            m_remarks.get(), m_joystickPort.get().toString(), m_speedModifier.get().toString(),
+            String.valueOf(m_canMove.get())));
     }
 
     
