4ff721dbb3b1b624ac3478eeab8cc1b8848409c2
Atai Ambus <atayambus@gmail.com>
2020-01-25 13:00:06 +0200

    feat(test): joystick testing
    - Bound all buttons to print themselves using the F310GamePad and ExtremeProJoystick classes, occasionally using GroupButtonBindings. 
    
    - Set left joystick exponential and right joystick inverted - we'll try driving straight to make sure this gmakes a difference.
    
    - Killed button joystick axes.
    
    - Used the constructor adjuster to multiply everything by 0.5
    
    - Set the right joystick's x axis to multiply by 0.1
    
    - Logged the joysticks on the shuffleboard.

--- src/main/java/com/evergreen/everlib/Test.java
+++ src/main/java/com/evergreen/everlib/Test.java
@@ -1,15 +1,20 @@
 package com.evergreen.everlib;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.evergreen.everlib.oi.joysticks.ExtremeProJoystick;
+import com.evergreen.everlib.oi.joysticks.F310GamePad;
+import com.evergreen.everlib.oi.joysticks.GroupButtonBindings;
+import com.evergreen.everlib.oi.joysticks.ExtremeProJoystick.X;
+import com.evergreen.everlib.oi.joysticks.ExtremeProJoystick.Y;
+import com.evergreen.everlib.oi.joysticks.ExtremeProJoystick.Z;
+import com.evergreen.everlib.oi.joysticks.F310GamePad.F310;
 import com.evergreen.everlib.shuffleboard.constants.ConstantDouble;
 import com.evergreen.everlib.shuffleboard.loggables.DashboardStreams;
 import com.evergreen.everlib.structure.Tree;
 import com.evergreen.everlib.subsystems.motors.subsystems.MotorController;
 import com.evergreen.everlib.subsystems.motors.subsystems.MotorController.ControllerType;
 
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Joystick.AxisType;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 
 /**
  * Test
@@ -19,85 +24,130 @@ public class Test extends Tree {
     
     MotorController m_motorRight = new MotorController(
         "Motor Right",
-        new MotorController("Talon", ControllerType.TALON_SRX, 9),
-        new MotorController("Victor", ControllerType.VICTOR_SPX, 0));
+        new MotorController("Right Talons", ControllerType.TALON_SRX, 0),
+        new MotorController("Right Victors", ControllerType.VICTOR_SPX, 1, 2));
 
     MotorController m_motorLeft = new MotorController(
         "Motor Left",
-        new MotorController("Talon", ControllerType.TALON_SRX, 1),
-        new MotorController("Victor", ControllerType.VICTOR_SPX, 4));
-
-    WPI_TalonSRX m_leftA = new WPI_TalonSRX(9);
-    WPI_TalonSRX m_rightA = new WPI_TalonSRX(1);
-    WPI_VictorSPX m_leftB = new WPI_VictorSPX(0);
-    WPI_VictorSPX m_rightB = new WPI_VictorSPX(4);
+        new MotorController("Left Talons", ControllerType.TALON_SRX, 3),
+        new MotorController("Right Victors", ControllerType.VICTOR_SPX, 4, 5));
 
     ConstantDouble m_speedModifier = new ConstantDouble("Speed", 0.5);
     ConstantDouble m_change = new ConstantDouble("Right Adjust", 1.2);
 
     ExtremeProJoystick m_jsLeft = new ExtremeProJoystick("Joystick Left", 0, (v) -> v * 0.5 * 0.85);
     ExtremeProJoystick m_jsRight = new ExtremeProJoystick("Joystick Right", 1, (v) -> v * 0.5);
-
-    ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+    F310GamePad m_jsButton = new F310GamePad("Joystick Button", 2);
   
     @Override
     protected void componentSetup() {
+        m_jsLeft.setInverted();
+        m_jsLeft.setExponential();
+
+        m_jsRight.setAxisAdjuster(AxisType.kX, (v) -> 0.1 * v); //TODO check this works
+        m_jsButton.kill(); //TODO check this returns 0.
     }
 
     @Override
     protected void bindButtons() {
-        // TODO Auto-generated method stub
+        //TODO Make sure positions are correct, works.
+
+        m_jsLeft.getButton(X.LEFT, Y.BACK, Z.BOTTOM).whenPressed(
+            new PrintCommand("Left, Back, Bottom"));
+        
+        m_jsLeft.getButton(X.RIGHT, Y.BACK, Z.BOTTOM).whenPressed(
+            new PrintCommand("Right, Back, Bottom"));
+        
+        m_jsLeft.getButton(X.LEFT, Y.MIDDLE, Z.BOTTOM).whenPressed(
+            new PrintCommand("Left, Middle, Bottom"));
+        
+        m_jsLeft.getButton(X.RIGHT, Y.MIDDLE, Z.BOTTOM).whenPressed(
+            new PrintCommand("Right, Middle, Bottom"));
+        
+        m_jsLeft.getButton(X.LEFT, Y.FORWARD, Z.BOTTOM).whenPressed(
+            new PrintCommand("Left, Forward, Bottom"));
+        
+        m_jsLeft.getButton(X.RIGHT, Y.FORWARD, Z.BOTTOM).whenPressed(
+            new PrintCommand("Right, Forward, Bottom"));
+        
+        m_jsLeft.getButton(X.LEFT, Y.BACK, Z.TOP).whenPressed(
+            new PrintCommand("Left, Back, Top"));
+        
+        m_jsLeft.getButton(X.RIGHT, Y.BACK, Z.TOP).whenPressed(
+            new PrintCommand("Right, Back, Top"));
+        
+        m_jsLeft.getButton(X.LEFT, Y.BACK, Z.TOP).whenPressed(
+            new PrintCommand("Left, Back, Top"));
+        
+        m_jsLeft.getButton(X.RIGHT, Y.BACK, Z.TOP).whenPressed(
+            new PrintCommand("Right, Back, Top"));
+        
+        m_jsLeft.getButton(X.LEFT, Y.MIDDLE, Z.TOP).whenPressed( //TODO make sure top middle throws an exception
+            new PrintCommand("Left, Middle, Top"));
+        
+        m_jsLeft.getButton(X.RIGHT, Y.MIDDLE, Z.TOP).whenPressed(
+            new PrintCommand("Right, Middle, Top"));
+        
+        m_jsLeft.getButton(X.LEFT, Y.FORWARD, Z.TOP).whenPressed(
+            new PrintCommand("Left, Forward, Top"));
+        
+        m_jsLeft.getButton(X.RIGHT, Y.FORWARD, Z.TOP).whenPressed(
+            new PrintCommand("Right, Forward, Top"));
+
+        GroupButtonBindings.whenPressed(
+            new PrintCommand("Trigger"), 
+            m_jsLeft.trigger(),
+            m_jsRight.trigger());
+        
+        GroupButtonBindings.whileHeld(
+            new PrintCommand("Thumb").perpetually(), 
+            m_jsLeft.thumb(),
+            m_jsRight.thumb());
+
+
+        for (F310 button : F310.values()) {
+            m_jsButton.getButton(button).whenPressed(new PrintCommand(button.toString()));
+        }
+
     }
 
     @Override
     protected void commandConfig() {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     protected void log() {
-        // DashboardStreams.addLoggable(m_motorLeft);
-        // DashboardStreams.addLoggable(m_motorRight);
-        // DashboardStreams.addDouble(
-        //     "Chassis Left Encoder", 
-        //     () -> (double)((WPI_TalonSRX)m_motorLeft.getMotors().get(0)).getSelectedSensorPosition());
-        DashboardStreams.addDouble(
-            "Talon Encoder", () -> m_leftA.getSelectedSensorPosition() * 1.0);
+        //TODO make sure all streams work.
+        DashboardStreams.addLoggable(m_jsButton);
+        DashboardStreams.addLoggable(m_jsLeft);
+        DashboardStreams.addLoggable(m_jsRight);
     }
 
     @Override
     protected void whenEnabled() {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     protected void autoConfig() {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     protected void teleopConfig() {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     protected void test() {
-        // TODO Auto-generated method stub
     }
 
     @Override
     public void testPeriodic() {
-        // m_motorLeft.set(m_jsLeft.getY());
-        // m_motorRight.set(m_jsRight.getY());
-        
-        m_leftA.set(-m_jsLeft.getY());
-        m_leftB.set(-m_jsLeft.getY());
-        m_rightA.set(m_jsRight.getY());
-        m_rightB.set(m_jsRight.getY());
+        //TODO try driving straight - if exponential works, than same pull won't do it.
+        m_motorLeft.set(m_jsLeft.getY());
+        m_motorRight.set(m_jsRight.getY());
     }
 
     
