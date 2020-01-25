632ef9aa1609f0c47f7764cbe27bd53a4096f261
Atai Ambus <atayambus@gmail.com>
2020-01-25 13:04:22 +0200

    feat(test): add basic objects
     - Added the basic motor and joystick objects.

--- src/main/java/com/evergreen/everlib/Test.java
+++ src/main/java/com/evergreen/everlib/Test.java
@@ -1,12 +1,30 @@
 package com.evergreen.everlib;
 
+import com.evergreen.everlib.oi.joysticks.ExtremeProJoystick;
+import com.evergreen.everlib.oi.joysticks.F310GamePad;
 import com.evergreen.everlib.structure.Tree;
+import com.evergreen.everlib.subsystems.motors.subsystems.MotorController;
+import com.evergreen.everlib.subsystems.motors.subsystems.MotorController.ControllerType;
 
 /**
  * Test
  */
 public class Test extends Tree {
 
+
+    MotorController m_motorRight = new MotorController(
+        "Motor Right",
+        new MotorController("Right Talons", ControllerType.TALON_SRX, 0),
+        new MotorController("Right Victors", ControllerType.VICTOR_SPX, 1, 2));
+
+    MotorController m_motorLeft = new MotorController(
+        "Motor Left",
+        new MotorController("Left Talons", ControllerType.TALON_SRX, 3),
+        new MotorController("Right Victors", ControllerType.VICTOR_SPX, 4, 5));
+
+    ExtremeProJoystick m_jsLeft = new ExtremeProJoystick("Joystick Left", 0, (v) -> v * 0.5);
+    ExtremeProJoystick m_jsRight = new ExtremeProJoystick("Joystick Right", 1, (v) -> v * 0.5);
+    F310GamePad m_jsButton = new F310GamePad("Joystick Button", 2);
 
     @Override
     protected void componentSetup() {
