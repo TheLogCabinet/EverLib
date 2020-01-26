227db772f52f0c814cab7ff3f9d2773549deb66d
Atai Ambus <atayambus@gmail.com>
2020-01-26 12:50:49 +0200

    feat(test): add command testing
    Added tests for SetConstant, SetConstantUntil. SetSwitch and ToggleSwitch, as well ats DashboardConstant's resetValues() and resetBoard().

--- src/main/java/com/evergreen/everlib/Test.java
+++ src/main/java/com/evergreen/everlib/Test.java
@@ -1,13 +1,20 @@
 package com.evergreen.everlib;
 
+import com.evergreen.everlib.oi.joysticks.F310GamePad;
+import com.evergreen.everlib.oi.joysticks.F310GamePad.F310;
 import com.evergreen.everlib.shuffleboard.constants.ConstantBoolean;
 import com.evergreen.everlib.shuffleboard.constants.ConstantDouble;
 import com.evergreen.everlib.shuffleboard.constants.ConstantInt;
 import com.evergreen.everlib.shuffleboard.constants.ConstantString;
 import com.evergreen.everlib.shuffleboard.constants.DashboardConstants;
+import com.evergreen.everlib.shuffleboard.constants.commands.SetConstant;
+import com.evergreen.everlib.shuffleboard.constants.commands.SetConstantUntil;
+import com.evergreen.everlib.shuffleboard.constants.commands.SetSwitch;
+import com.evergreen.everlib.shuffleboard.constants.commands.ToggleSwitch;
 import com.evergreen.everlib.structure.Tree;
 import com.evergreen.everlib.subsystems.motors.subsystems.MotorController;
 import com.evergreen.everlib.subsystems.motors.subsystems.MotorController.ControllerType;
+import com.evergreen.everlib.utils.InstantCommandEG;
 
 import edu.wpi.first.wpilibj.Joystick;
 
@@ -17,7 +24,7 @@ import edu.wpi.first.wpilibj.Joystick;
 public class Test extends Tree {
     
     
-    MotorController m_chassis = new MotorController( //TODO Check ports!
+    MotorController m_chassis = new MotorController( //                                                                                                                                                                 `   ```             
         "Motor Right",
         new MotorController("Left Front", ControllerType.TALON_SRX, 9),
         new MotorController("Left Back", ControllerType.VICTOR_SPX, 0),
@@ -25,6 +32,8 @@ public class Test extends Tree {
         new MotorController("Rught Back", ControllerType.VICTOR_SPX, 4),
         new MotorController("Left Up", ControllerType.VICTOR_SPX, 0),
         new MotorController("Right Up", ControllerType.VICTOR_SPX, 1));
+    
+    F310GamePad m_gamePad = new F310GamePad("Joystick Button", 0);
 
     static {
         DashboardConstants.getInstance().startConstantsOf("subsystemName");
@@ -57,10 +66,33 @@ public class Test extends Tree {
   
     @Override
     protected void componentSetup() {
+
     }
 
     @Override
     protected void bindButtons() {
+        m_gamePad.getButton(F310.X).whenPressed(new SetConstant(
+            "Increase Speed", m_speedModifier, () -> m_speedModifier.get() + 0.1));
+
+        m_gamePad.getButton(F310.B).whenPressed(new SetConstant(
+            "Decrease Speed", m_speedModifier, () -> m_speedModifier.get() - 0.1));
+
+        m_gamePad.getButton(F310.Y).whenPressed(new SetSwitch("Enable", true, m_canMove));
+        m_gamePad.getButton(F310.A).whenPressed(new SetSwitch("Disable", false, m_canMove));
+
+        m_gamePad.getButton(F310.JOYSTICK_LEFT).whenPressed(new ToggleSwitch("Toggle", m_canMove));
+
+        m_gamePad.getButton(F310.RT).whileHeld(new SetConstantUntil(
+            "Fast Drive", m_speedModifier, () -> 0.7));
+
+        m_gamePad.getButton(F310.RT).whileHeld(new SetConstantUntil(
+            "Slow Drive", m_speedModifier, () -> 0.2, () -> !m_canMove.get()));
+
+        m_gamePad.getButton(F310.START).whenPressed(new InstantCommandEG(
+            "Reset Shuffleboard", () -> DashboardConstants.getInstance().resetValues()));
+
+        m_gamePad.getButton(F310.START).whenPressed(new InstantCommandEG(
+            "Clean Shuffleboard", () -> DashboardConstants.getInstance().resetBoard()));
     }
 
     @Override
