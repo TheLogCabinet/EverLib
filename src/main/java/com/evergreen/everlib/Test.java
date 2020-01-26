db1f76940794b9e0f832a5d79f7762c03f301527
Atai Ambus <atayambus@gmail.com>
2020-01-26 13:33:09 +0200

    feat(test): add CommandEG testing
     - Added an InsatntCommandEG which prints "Test", and schedualed it on each robot enable.
    
    - Toggled the command on left thumb, enabled it on left trigger and disabled it on right trigger

--- src/main/java/com/evergreen/everlib/Test.java
+++ src/main/java/com/evergreen/everlib/Test.java
@@ -2,9 +2,11 @@ package com.evergreen.everlib;
 
 import com.evergreen.everlib.oi.joysticks.ExtremeProJoystick;
 import com.evergreen.everlib.oi.joysticks.F310GamePad;
+import com.evergreen.everlib.shuffleboard.loggables.DashboardStreams;
 import com.evergreen.everlib.structure.Tree;
 import com.evergreen.everlib.subsystems.motors.subsystems.MotorController;
 import com.evergreen.everlib.subsystems.motors.subsystems.MotorController.ControllerType;
+import com.evergreen.everlib.utils.InstantCommandEG;
 
 /**
  * Test
@@ -26,9 +28,11 @@ public class Test extends Tree {
     ExtremeProJoystick m_jsRight = new ExtremeProJoystick("Joystick Right", 1, (v) -> v * 0.5);
     F310GamePad m_jsButton = new F310GamePad("Joystick Button", 2);
 
+    CommandEG m_testCommand = new InstantCommandEG("Test Command", () -> System.out.println("Test"));
+
     @Override
     protected void componentSetup() {
-
+         
     }
 
     @Override
@@ -39,17 +43,16 @@ public class Test extends Tree {
     @Override
     protected void commandConfig() {
        
-
     }
 
     @Override
     protected void log() {
-
+        DashboardStreams.addLoggable(m_testCommand);
     }
 
     @Override
     protected void whenEnabled() {
-       
+       m_testCommand.schedule();
 
     }
 
@@ -72,7 +75,10 @@ public class Test extends Tree {
 
     @Override
     public void testPeriodic() {
+        if (m_jsLeft.getRawButtonPressed(1)) m_testCommand.enable();
+        if (m_jsRight.getRawButtonPressed(1)) m_testCommand.disable();
 
+        if (m_jsLeft.getRawButtonPressed(2)) m_testCommand.toggle();
     }
 
     
