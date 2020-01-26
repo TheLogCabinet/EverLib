72c73af8fbd4cb704ca7558f4d88d25c723285ce
Atai Ambus <atayambus@gmail.com>
2020-01-26 15:36:55 +0200

    feat(test): add range testing
    - Test shuffleboard constant against lambda, max, min and double limits, as well as limitless, all their values hshuffleboard suppliers. 
    
    - Streams results to shuffleboard

--- src/main/java/com/evergreen/everlib/Test.java
+++ src/main/java/com/evergreen/everlib/Test.java
@@ -1,30 +1,37 @@
 package com.evergreen.everlib;
 
-import com.evergreen.everlib.oi.joysticks.ExtremeProJoystick;
-import com.evergreen.everlib.oi.joysticks.F310GamePad;
+import com.evergreen.everlib.shuffleboard.constants.ConstantDouble;
+import com.evergreen.everlib.shuffleboard.loggables.DashboardStreams;
 import com.evergreen.everlib.structure.Tree;
-import com.evergreen.everlib.subsystems.motors.subsystems.MotorController;
-import com.evergreen.everlib.subsystems.motors.subsystems.MotorController.ControllerType;
+import com.evergreen.everlib.utils.ranges.DoubleLimit;
+import com.evergreen.everlib.utils.ranges.Limitless;
+import com.evergreen.everlib.utils.ranges.MaxLimit;
+import com.evergreen.everlib.utils.ranges.MinLimit;
+import com.evergreen.everlib.utils.ranges.Range;
 
 /**
  * Test
  */
 public class Test extends Tree {
 
+    ConstantDouble m_maxLimit = new ConstantDouble("Max Limit", -5);
+    ConstantDouble m_minLimit = new ConstantDouble("Min Limit", 5);
+    ConstantDouble m_tolerance = new ConstantDouble("Tolerance", 1);
 
-    MotorController m_motorRight = new MotorController(
-        "Motor Right",
-        new MotorController("Right Talons", ControllerType.TALON_SRX, 0),
-        new MotorController("Right Victors", ControllerType.VICTOR_SPX, 1, 2));
+    ConstantDouble m_testAgainst = new ConstantDouble("Testing Against", 0);
 
-    MotorController m_motorLeft = new MotorController(
-        "Motor Left",
-        new MotorController("Left Talons", ControllerType.TALON_SRX, 3),
-        new MotorController("Right Victors", ControllerType.VICTOR_SPX, 4, 5));
+    Range m_lambdaRange = (v) -> (Math.pow(v, 2) + 4 * v - 2 ) > 0;
 
-    ExtremeProJoystick m_jsLeft = new ExtremeProJoystick("Joystick Left", 0, (v) -> v * 0.5);
-    ExtremeProJoystick m_jsRight = new ExtremeProJoystick("Joystick Right", 1, (v) -> v * 0.5);
-    F310GamePad m_jsButton = new F310GamePad("Joystick Button", 2);
+    Range m_maxRange = new MaxLimit(m_maxLimit);  
+    Range m_tolerantMaxRange = new MaxLimit(m_maxLimit, m_tolerance); 
+
+    Range m_minRange = new MinLimit(m_minLimit);  
+    Range m_tolerantMinRange = new MinLimit(m_minLimit, m_tolerance); 
+
+    Range m_doubleRange = new DoubleLimit(m_maxLimit, m_minLimit);  
+    Range m_tolerantDoubleRange = new DoubleLimit(m_maxLimit, m_minLimit, m_tolerance);
+
+    Range m_limitless = new Limitless();
 
     @Override
     protected void componentSetup() {
@@ -45,6 +52,25 @@ public class Test extends Tree {
     @Override
     protected void log() {
 
+        DashboardStreams.addBoolean(
+            "In Range - Lambda", 
+            () -> m_lambdaRange.inRange(m_testAgainst.get()));
+        
+        DashboardStreams.addBoolean(
+            "In Range - Min", 
+            () -> m_minRange.inRange(m_testAgainst.get()));
+        
+        DashboardStreams.addBoolean(
+            "In Range - Max", 
+            () -> m_maxRange.inRange(m_testAgainst.get()));
+    
+        DashboardStreams.addBoolean(
+            "In Range - Double", 
+            () -> m_doubleRange.inRange(m_testAgainst.get()));
+    
+        DashboardStreams.addBoolean(
+            "In Range - Limitless", 
+            () -> m_limitless.inRange(m_testAgainst.get()));
     }
 
     @Override
