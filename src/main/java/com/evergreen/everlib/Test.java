0bd287e4e3200144e43c36e18d0685a3426e1be7
Atai Ambus <atayambus@gmail.com>
2020-01-26 13:53:05 +0200

    feat(test): added tree function tests
    Added prints to see all tree functions run when they should.

--- src/main/java/com/evergreen/everlib/Test.java
+++ src/main/java/com/evergreen/everlib/Test.java
@@ -1,78 +1,78 @@
 package com.evergreen.everlib;
 
-import com.evergreen.everlib.oi.joysticks.ExtremeProJoystick;
-import com.evergreen.everlib.oi.joysticks.F310GamePad;
+import java.util.List;
+
 import com.evergreen.everlib.structure.Tree;
-import com.evergreen.everlib.subsystems.motors.subsystems.MotorController;
-import com.evergreen.everlib.subsystems.motors.subsystems.MotorController.ControllerType;
+import com.evergreen.everlib.utils.InstantCommandEG;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Test
  */
 public class Test extends Tree {
 
-
-    MotorController m_motorRight = new MotorController(
-        "Motor Right",
-        new MotorController("Right Talons", ControllerType.TALON_SRX, 0),
-        new MotorController("Right Victors", ControllerType.VICTOR_SPX, 1, 2));
-
-    MotorController m_motorLeft = new MotorController(
-        "Motor Left",
-        new MotorController("Left Talons", ControllerType.TALON_SRX, 3),
-        new MotorController("Right Victors", ControllerType.VICTOR_SPX, 4, 5));
-
-    ExtremeProJoystick m_jsLeft = new ExtremeProJoystick("Joystick Left", 0, (v) -> v * 0.5);
-    ExtremeProJoystick m_jsRight = new ExtremeProJoystick("Joystick Right", 1, (v) -> v * 0.5);
-    F310GamePad m_jsButton = new F310GamePad("Joystick Button", 2);
+    double m_testVal = 0;
 
     @Override
     protected void componentSetup() {
-
+        System.out.println("Seting Up Components... "); //RobotInit
     }
 
     @Override
     protected void bindButtons() {
-       
+       System.err.println("Bindind Buttons..."); //Robot Init
     }
 
     @Override
     protected void commandConfig() {
-       
-
+        System.out.println("Configuring Commands"); //Robot Init
     }
 
     @Override
     protected void log() {
-
+        System.out.println("Adding Loggable Streams"); //Robot Init
     }
 
     @Override
-    protected void whenEnabled() {
-       
-
+    protected void whenEnabled() { //TestInit, AutonomusInit
+        System.out.println("Running Enabled State");
     }
 
     @Override
-    protected void autoConfig() {
-       
-
+    protected void autoConfig() { //Autonomus Init       
+        System.out.println("Configuring Autonomus Commands");
     }
 
     @Override
-    protected void teleopConfig() {
-       
-
+    protected void teleopConfig() { //Teleop Init
+       System.out.println("Configuring Teleop Commands");
     }
 
     @Override
-    protected void test() {
-       
+    protected void test() { //TestInit
+       System.out.println("Testing...");
     }
 
     @Override
-    public void testPeriodic() {
+    protected void update() { //RobotPeriodic
+        SmartDashboard.putNumber("Update Test", ++m_testVal);
+    }
 
+    @Override
+    protected List<CommandEG> getAutoCommands() { //Runs in AutoInit
+        return List.of(
+            new InstantCommandEG("Auto Printer #0", () -> System.out.println("AUTO COMMAND #0")),
+            new InstantCommandEG("Auto Printer #1", () -> System.out.println("AUTO COMMAND #1"))
+        );
+    }
+
+    @Override
+    protected List<CommandEG> getTeleopCommands() { //Runs in AutoInit
+        return List.of(
+            new InstantCommandEG("Teleop Printer #0", () -> System.out.println("TELEOP COMMAND #0")),
+            new InstantCommandEG("Teleop Printer #1", () -> System.out.println("TELEOP COMMAND #1"))
+        );
     }
 
     
